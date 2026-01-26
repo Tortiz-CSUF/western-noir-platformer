ed76562bf1f32027dc0b5695652f00e17c522d3f
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-01-25 22:07:21 -0800

    Refactoring Movement Script
    Refactoring Player Movement script for intended use. Moved Cmaera2D into Player Hierarchy since it will follow the player.

--- Player_Movement.gd
+++ Player_Movement.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var cam: Camera2D = $Camera2D
 
 
 func _physics_process(delta: float) -> void:
@@ -11,15 +13,20 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	# Horizontal movement
+	var dir := Input.get_axis("move_left", "move_right")
+	velocity.x = dir * SPEED
+	
 	move_and_slide()
+	
+	_update_anim(dir)
+	
+func _update_anim(dir: float) -> void
+	#flip sprite to correspond to movement direction
+	if dir != 0.0:
+			anim.flip
+	
+	
