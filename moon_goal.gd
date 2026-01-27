b2ff6d351dd0091d0d68f33b55ecd068de9d1346
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-01-26 21:41:10 -0800

    moon_goal script
    - Created and implemented (unfinished) script that will serve as a trigger for the Moon Goal to display the "Level Complete" UI message.
    
    Flow: Detects body has entered area -> freezes movement -> displays UI -> message

---
+++ moon_goal.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+@export_multiline var CLEARANCE_MESSAGE : String = "Level Complete!"
+@export var UI_PATH: NodePath
+@export var PLAYER_GROUP: String = "Player"
+
+var triggered := false
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+
+func _on_body_entered(body: Node) -> void:
+	if triggered:
+		return 
+	if not body.is_in_group(PLAYER_GROUP):
+		return
+		
+# Stop player movement to dislay UI
+if body.has_method("freeze"):
+	body.freeze()
+	
+var ui :=
