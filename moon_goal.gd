7e1ef3cd6dde68042706e951dece364548d6df8d
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-01-26 22:46:17 -0800

    Update to moon_goal script
    - Refactored moon_goal script to properly assign Player node. Previously had path issue.
    
    - Tested refactored script. Now displays Ui message.
    
    *Need to add movement freeze to player_movement script

--- moon_goal.gd
+++ moon_goal.gd
@@ -2,25 +2,31 @@ extends Area2D
 
 @export_multiline var CLEARANCE_MESSAGE : String = "Level Complete!"
 @export var UI_PATH: NodePath
-@export var PLAYER_GROUP: String = "Player"
+@export var PLAYER_PATH: NodePath
 
 var triggered := false
+var player: Node
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
-
+	
+	player = get_node_or_null(PLAYER_PATH)
+	if player == null:
+		push_warning('MoonGoal: PLAYER_PATH not set or invalid...')
 
 func _on_body_entered(body: Node) -> void:
 	if triggered:
 		return 
-	if not body.is_in_group(PLAYER_GROUP):
+	if body != player:
 		return
 		
+	triggered = true
+	
 	# Stop player movement to dislay UI
-	if body.has_method("freeze"):
-		body.freeze()
+	if player.has_method("freeze"):
+		player.freeze()
 	
 	# Display Clearance UI
 	var ui := get_node_or_null(UI_PATH)
-	if ui != null:
+	if ui != null and ui.has_method("show_message"):
 		ui.show_message(CLEARANCE_MESSAGE)
