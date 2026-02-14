ef9849faf62e828ed6bc1a1b4dfaf7f4d697c868
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-02-14 04:44:13 +0000

    Debugged pause_menu script
    - debugged pause_menu to be visible when intended inlevel scenes.
    - disabled pause_menu ability to be called during main menu.
    - made sure pause_menu is hidden when restart and main menu options are selected.

--- pause_menu.gd
+++ pause_menu.gd
@@ -10,8 +10,15 @@ func _ready() -> void:
 	root.visible = false
 	print("[Pause Menu]: ready. root path =", root.get_path())
 	
-func _unhandled_input(event: InputEvent) -> void:
+func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
+		var current := get_tree().current_scene
+		
+		# not allowed in main menu
+		if current != null and main_menu_scene != null:
+			if current.scene_file_path == main_menu_scene.resource_path:
+				return
+						
 		toggle_pause()
 		get_viewport().set_input_as_handled()
 	
@@ -27,6 +34,7 @@ func _on_resume_button_pressed() -> void:
 func _on_restart_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().reload_current_scene()
+	close_pause_menu()
 
 func _on_main_menu_button_pressed() -> void:
 	get_tree().paused = false
@@ -34,6 +42,14 @@ func _on_main_menu_button_pressed() -> void:
 		get_tree().change_scene_to_packed(main_menu_scene)
 	else:
 		push_warning(" PauseMenu: main_menu not set.")
+		
+	close_pause_menu()
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+### helper function
+func close_pause_menu() -> void:
+	get_tree().paused = false
+	root.visible = false
