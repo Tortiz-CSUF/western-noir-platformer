ce407c78ee6cdfe3089fc782e449a23ba11d4816
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-02-14 04:05:34 +0000

    Completed Pause Menu Script
    Finished pause menu script:
    - added all button press logic.

--- pause_menu.gd
+++ pause_menu.gd
@@ -14,25 +14,35 @@ func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	root.visible = false
 	
-	resume_button.pressed.connect(())
+	resume_button.pressed.connect(_on_resume_button_pressed)
+	restart_button.pressed.connect(_on_restart_button_pressed)
+	main_menu_button.pressed.connect(_on_main_menu_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		toggle_pause()
+		get_viewport().set_input_as_handled()
+	
+func toggle_pause() -> void:
+	var now_paused := not get_tree().paused
+	get_tree().paused = now_paused
+	root.visible = false
 
 func _on_resume_button_pressed() -> void:
-	pass # Replace with function body.
-
+	get_tree().paused = false
+	root.visible = false
 
 func _on_restart_button_pressed() -> void:
-	pass # Replace with function body.
-
+	get_tree().paused = false
+	get_tree().reload_current_scene()
 
 func _on_main_menu_button_pressed() -> void:
-	pass # Replace with function body.
-
+	get_tree().paused = false
+	if main_menu_scene:
+		get_tree().change_scene_to_packed(main_menu_scene)
+	else:
+		push_warning(" PauseMenu: main_menu not set.")
 
 func _on_quit_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().quit()
