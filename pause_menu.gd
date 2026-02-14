720b355b083ec775f85bfe02269ae16337c3392b
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-02-14 04:28:09 +0000

    Refactored pause_menu script and debugging
    Debugging pause menu not showing as intended.

--- pause_menu.gd
+++ pause_menu.gd
@@ -3,22 +3,13 @@ extends CanvasLayer
 @export var main_menu_scene: PackedScene
 
 @onready var root: Control = $Root
-@onready var resume_button: Button = $Root/Panel/VBox/ResumeButton
-@onready var restart_button: Button = $Root/Panel/VBox/RestartButton
-@onready var main_menu_button: Button = $Root/Panel/VBox/MainMenuButton
-@onready var quit_button: Button = $Root/Panel/VBox/QuitButton
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	root.visible = false
+	print("[Pause Menu]: ready. root path =", root.get_path())
 	
-	resume_button.pressed.connect(_on_resume_button_pressed)
-	restart_button.pressed.connect(_on_restart_button_pressed)
-	main_menu_button.pressed.connect(_on_main_menu_button_pressed)
-	quit_button.pressed.connect(_on_quit_button_pressed)
-
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		toggle_pause()
@@ -27,7 +18,7 @@ func _unhandled_input(event: InputEvent) -> void:
 func toggle_pause() -> void:
 	var now_paused := not get_tree().paused
 	get_tree().paused = now_paused
-	root.visible = false
+	root.visible = now_paused
 
 func _on_resume_button_pressed() -> void:
 	get_tree().paused = false
