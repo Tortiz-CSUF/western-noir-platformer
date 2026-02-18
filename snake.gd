bfa68bc9cd9491b09f91d52957131ee41c3a3f13
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-02-18 22:47:34 +0000

    Snake Script Complete
    Completed Script for Snake Enemy AI:
    - Sprite flip logic
    - animation logic
    - gravity logic

--- snake.gd
+++ snake.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 @export var aggro_range: float = 150.0
 @export var damage: int = 1
 @export var damage_cooldown: float = 0.6
+@export var gravity: float = 900.0
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hurtbox: Area2D = $Hurtbox
@@ -16,18 +17,21 @@ func _ready() -> void:
 	# Hurtbox used to detect collision against player
 	hurtbox.body_entered.connect(_on_hurt_body_entered)
 	
+	#flip sprite to intended direction 
+	anim.flip_h = false
+	
 func _physics_process(delta: float) -> void:
 	_player = _find_player()
 	
 	var desired_vel_x := 0.0
+	var dir_x := 0.0
 	
 	if _player != null:
 		var dist := global_position.distance_to(_player.global_position)
 		if dist <= aggro_range:
-			var dir := sign(_player.global_position.x - global_position.x)
-			desired_vel_x = dir * move_speed
-			
-			_play_walk(dir)
+			dir_x = sign(_player.global_position.x - global_position.x)
+			desired_vel_x = dir_x * move_speed
+			_play_walk(dir_x)
 		else: 
 			_play_idle()
 	else: 
@@ -35,10 +39,24 @@ func _physics_process(delta: float) -> void:
 	
 	velocity.x = desired_vel_x
 	
-
-func _play_idle() -> void:
+	#gravity logic
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		velocity.y = 0.0
+		
+	move_and_slide()
 	
-func _play_walk(dir: float) -> void:
+func _play_idle() -> void:
+	if anim.animation != "idle":
+		anim.play("idle")
+	
+func _play_walk(dir_x: float) -> void:
+	if anim.animation != "walk":
+		anim.play("walk")
+	#flip sprite	
+	if dir_x != 0.0:
+		anim.flip_h = (dir_x > 0.0)
 
 func _find_player() -> Node2D:
 	var players := get_tree().get_nodes_in_group("player")
